2e23d497aefc1f94f92536178273c730a1e14bab
bonnie <bonnieqing37@gmail.com>
2025-05-22 07:38:43 +0800

    Format code

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ## How to set up
+
 1. install dependency
+
 ```sh
 pnpm i
 ```
 
 2. launch server
+
 ```
 pnpm run dev
 ```
--- src/agent/state.ts
+++ src/agent/state.ts
@@ -6,4 +6,4 @@ export const GraphState = Annotation.Root({
     reducer: (x, y) => x.concat(y),
     default: () => [],
   }),
-})
+});
